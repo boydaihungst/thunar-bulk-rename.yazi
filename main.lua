75e9eaafe0dbd8efc1099f10c867787ffc93930f
Huy Hoang <boydaihungst@gmail.com>
2025-12-04 11:26:02 +0700

    fix: fallback to default bulk rename when rename vfs remote files

--- main.lua
+++ main.lua
@@ -3,13 +3,20 @@
 
 return {
 	entry = function()
+		ya.emit("escape", { visual = true })
 		local selected_items = cx.active.selected
 		if #selected_items >= 1 then
 			local selected_urls = ""
 			for _, v in pairs(selected_items) do
+				local is_virtual = v.scheme and v.scheme.is_virtual
+				if is_virtual then
+					ya.dbg("thunar-bulk-rename", "Thunar rename doesn't support virtual file system")
+					ya.emit("rename", { hovered = false, cursor = "before_ext" })
+					return
+				end
 				selected_urls = selected_urls .. ya.quote(tostring(v))
 			end
-			ya.emit("shell", { 'thunar -B "$@"', block = true, confirm = true })
+			ya.emit("shell", { "thunar -B %s", block = true, confirm = true })
 		else
 			ya.emit("rename", { hovered = false, cursor = "before_ext" })
 		end
