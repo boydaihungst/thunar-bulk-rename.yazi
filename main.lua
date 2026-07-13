8fdf9c669e6601063b6575afc8a5c51396f02a11
Huy Hoang <boydaihungst@gmail.com>
2026-07-13 20:16:01 +0700

    fix: adapt to new yazi API: url.spec

--- main.lua
+++ main.lua
@@ -7,9 +7,9 @@ return {
 		local selected_items = cx.active.selected
 		if #selected_items >= 1 then
 			local selected_urls = ""
-			for _, u in pairs(selected_items) do
+			for _, f in pairs(selected_items) do
 				local u = f.url or f
-				local is_virtual = u.scheme and u.scheme.is_virtual
+				local is_virtual = (u.spec and u.spec.is_virtual) or (u.scheme and u.scheme.is_virtual)
 				if is_virtual then
 					ya.dbg("thunar-bulk-rename", "Thunar rename doesn't support virtual file system")
 					ya.emit("rename", { hovered = false, cursor = "before_ext" })
